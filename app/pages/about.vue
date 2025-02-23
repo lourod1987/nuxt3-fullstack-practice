feb9a3244b5af6c0f5ac9cd661fa240d4dc957b9
Luis Rodriguez III <luis.rodriguez@traderinteractive.com>
2025-02-23 15:51:18 -0500

    Basic setup of portfolio site with simple styles and dark mode

---
+++ app/pages/about.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    This page is about me.
+  </div>
+  <!-- <div>
+    <button @click="enableCustomLayout">Update Layout</button>
+  </div> -->
+</template>
+
+<script setup>
+  useHead({
+    title: 'About',
+  });
+  // definePageMeta({
+  //   layout: "another",
+  // });
+  // function enableCustomLayout() {
+  //   setPageLayout('another');
+  // }
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,3 @@
+module.export = {
+  darkMode: 'class',
+};
